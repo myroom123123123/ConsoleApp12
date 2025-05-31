64a9a5cfb7ce8075a498aaefe2272c1ba7439072
Petre_to82@student.itstep.org <Petre_to82@student.itstep.org>
2025-05-31 12:49:59 +0300

    ex1

---
+++ classes/ArrayProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12.classes
+{
+    // Делегат для фільтрації чисел
+    public delegate bool NumberFilter(int number);
+
+    public class ArrayProcessor
+    {
+        private int[] _numbers;
+
+        public ArrayProcessor(int[] numbers)
+        {
+            _numbers = numbers ?? throw new ArgumentNullException(nameof(numbers));
+        }
+
+        // Метод для фільтрації чисел з використанням делегата
+        public int[] FilterNumbers(NumberFilter filter)
+        {
+            return _numbers.Where(n => filter(n)).ToArray();
+        }
+
+        // Метод для виведення масиву
+        public void PrintArray(string title, int[] array)
+        {
+            Console.WriteLine($"{title}: [{string.Join(", ", array)}]");
+        }
+    }
+}
---
+++ classes/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp12.classes
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            var processor = new ArrayProcessor(numbers);
+
+            // Використання делегатів для фільтрації
+            var evenNumbers = processor.FilterNumbers(NumberFilters.IsEven);
+            var oddNumbers = processor.FilterNumbers(NumberFilters.IsOdd);
+            var primeNumbers = processor.FilterNumbers(NumberFilters.IsPrime);
+            var fibonacciNumbers = processor.FilterNumbers(NumberFilters.IsFibonacci);
+
+            // Виведення результатів
+            processor.PrintArray("Вихідний масив", numbers);
+            processor.PrintArray("Парні числа", evenNumbers);
+            processor.PrintArray("Непарні числа", oddNumbers);
+            processor.PrintArray("Прості числа", primeNumbers);
+            processor.PrintArray("Числа Фібоначчі", fibonacciNumbers);
+
+            // Альтернативний спосіб використання через лямбда-вирази
+            Console.WriteLine("\nВикористання лямбда-виразів:");
+            var multiplesOf3 = processor.FilterNumbers(n => n % 3 == 0);
+            processor.PrintArray("Числа, кратні 3", multiplesOf3);
+        }
+    }
+}
